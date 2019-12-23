159594a2f2d3446594eb19f60eda0e6b0ab8b632
yangchong <yangchong211@163.com>
2019-12-23 19:27:44 +0800

    提交代码，功能更新中

--- YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/web/OnDecorationListener.java
+++ YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/web/OnDecorationListener.java
@@ -2,9 +2,17 @@ package com.ns.yc.yccustomtextlib.web;
 
 import java.util.List;
 
+/**
+ * <pre>
+ *     @author 杨充
+ *     blog  : https://github.com/yangchong211
+ *     time  : 2019/07/18
+ *     desc  : 日志工具类
+ *     revise:
+ * </pre>
+ */
 public interface OnDecorationListener {
 
     void onStateChangeListener(String text, List<WebRichType> types);
-
 
 }
--- YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/web/OnTextChangeListener.java
+++ YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/web/OnTextChangeListener.java
@@ -4,5 +4,4 @@ public interface OnTextChangeListener {
 
     void onTextChange(String text);
 
-
 }
