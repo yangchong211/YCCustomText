38257ab6aa8c295311c414539a4280741700c8fc
yangchong <yangchong211@163.com>
2019-12-23 11:12:52 +0800

    提交代码，功能更新中

--- YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/model/HyperEditData.java
+++ YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/model/HyperEditData.java
@@ -8,12 +8,18 @@ import java.io.Serializable;
  *     blog  : https://github.com/yangchong211
  *     time  : 2016/3/31
  *     desc  : 富文本实体类
- *     revise:
+ *     revise: 目前就支持简单的富文本，文字+图片
  * </pre>
  */
 public class HyperEditData implements Serializable {
 
+    /**
+     * 富文本输入文字内容
+     */
     private String inputStr;
+    /**
+     * 富文本输入图片地址
+     */
     private String imagePath;
 
     public String getInputStr() {
