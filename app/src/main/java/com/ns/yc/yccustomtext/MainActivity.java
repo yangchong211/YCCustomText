cbb707ff24fede30d999fd1b7c7bbe2c7fbb019b
yangchong <yangchong211@163.com>
2019-12-24 18:42:42 +0800

    提交代码，功能更新中

--- app/src/main/java/com/ns/yc/yccustomtext/MainActivity.java
+++ app/src/main/java/com/ns/yc/yccustomtext/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ns.yc.yccustomtext;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,6 +32,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tv_2 = (TextView) findViewById(R.id.tv_2);
         tv_2.setOnClickListener(this);
 
+        tv_3 = (TextView) findViewById(R.id.tv_3);
+        tv_3.setOnClickListener(this);
+
     }
 
     @Override
@@ -43,6 +45,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.tv_2:
                 startActivity(new Intent(MainActivity.this,WebRichActivity.class));
+                break;
+            case R.id.tv_3:
+                startActivity(new Intent(MainActivity.this, WebTextActivity.class));
                 break;
             default:
                 break;
---
+++ app/src/main/java/com/ns/yc/yccustomtext/WebTextActivity.java
@@ -0,0 +1,68 @@
+package com.ns.yc.yccustomtext;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+public class WebTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_diarys);
+        final String dataStr = "http://sctest-ylyc-app.cheoo.com/edit/edit.html";
+        initWebView(dataStr);
+    }
+
+    public void initWebView(String data) {
+        WebView mWebView = findViewById(R.id.showdiarys);
+        WebSettings settings = mWebView.getSettings();
+
+        //settings.setUseWideViewPort(true);//调整到适合webview的大小，不过尽量不要用，有些手机有问题
+        settings.setLoadWithOverviewMode(true);//设置WebView是否使用预览模式加载界面。
+        mWebView.setVerticalScrollBarEnabled(false);//不能垂直滑动
+        mWebView.setHorizontalScrollBarEnabled(false);//不能水平滑动
+        settings.setTextSize(WebSettings.TextSize.NORMAL);//通过设置WebSettings，改变HTML中文字的大小
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);//支持通过JS打开新窗口
+        //设置WebView属性，能够执行Javascript脚本
+        mWebView.getSettings().setJavaScriptEnabled(true);//设置js可用
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.addJavascriptInterface(new AndroidJavaScript(getApplication()), "android");//设置js接口
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);//支持内容重新布局
+
+        mWebView.loadUrl(data);
+    }
+
+    /**
+     * AndroidJavaScript
+     * 本地与h5页面交互的js类，这里写成内部类了
+     * returnAndroid方法上@JavascriptInterface一定不能漏了
+     */
+    private class AndroidJavaScript {
+        Context mContxt;
+
+        public AndroidJavaScript(Context mContxt) {
+            this.mContxt = mContxt;
+        }
+
+        @JavascriptInterface
+        public void returnAndroid(String name) {//从网页跳回到APP，这个方法已经在上面的HTML中写上了
+            if (name.isEmpty() || name.equals("")) {
+                return;
+            }
+            Toast.makeText(getApplication(), name, Toast.LENGTH_SHORT).show();
+            //这里写你的操作///////////////////////
+            //MainActivity就是一个空页面，不影响
+            Intent intent = new Intent(WebTextActivity.this, MainActivity.class);
+            intent.putExtra("name", name);
+            startActivity(intent);
+        }
+    }
+}
