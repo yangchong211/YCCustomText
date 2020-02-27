7d8650e77cbbc0de06380274779e48150ba74e78
yangchong <yangchong211@163.com>
2020-02-27 14:18:59 +0800

    提交代码，功能更新中

--- YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/edit/manager/HyperManager.java
+++ YCCustomTextLib/src/main/java/com/ns/yc/yccustomtextlib/edit/manager/HyperManager.java
@@ -30,12 +30,21 @@ import com.ns.yc.yccustomtextlib.edit.view.HyperImageView;
  */
 public final class HyperManager {
 
-    private static HyperManager instance;
+    private static volatile HyperManager instance;
     private ImageLoader imageLoader;
 
+    /**
+     * 使用dcl双重校验方式获取单利对象
+     * 优点：在并发量不多，安全性不高的情况下或许能很完美运行单例模式
+     * 缺点：不同平台编译过程中可能会存在严重安全隐患。
+     *      在JDK1.5之后，官方给出了volatile关键字可以解决，多线程下执行顺序
+     * @return                          返回对象
+     */
     public static HyperManager getInstance(){
+        //第一层判断是为了避免不必要的同步
         if (instance == null){
             synchronized (HyperManager.class){
+                //第二层的判断是为了在null的情况下才创建实例
                 if (instance == null){
                     instance = new HyperManager();
                 }
@@ -44,13 +53,29 @@ public final class HyperManager {
         return instance;
     }
 
+    /*
+     * 利用面向对象思想，将图片的设置和加载都使用manager完成，让代码逻辑和业务逻辑分离
+     */
+
+
+    /**
+     * 设置加载loader
+     * @param imageLoader                   loader
+     */
     public void setImageLoader(ImageLoader imageLoader){
         this.imageLoader = imageLoader;
     }
 
+    /**
+     * 设置图片
+     * @param imagePath                     地址
+     * @param imageView                     图片view
+     * @param imageHeight                   图片高度
+     */
     public void loadImage(String imagePath, HyperImageView imageView, int imageHeight){
         if (imageLoader != null){
             imageLoader.loadImage(imagePath, imageView, imageHeight);
         }
     }
+
 }
